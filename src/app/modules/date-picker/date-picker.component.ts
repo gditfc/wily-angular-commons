f5d0f68720948fcc85cc12f96f1fec3cfc79e647
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-02-24 12:57:43 -0500

    Hook up today highlighting on calendar

--- src/app/modules/date-picker/date-picker.component.ts
+++ src/app/modules/date-picker/date-picker.component.ts
@@ -6,6 +6,12 @@ import { addDays, endOfMonth, subDays } from 'date-fns';
  * with extended metadata
  */
 declare interface MetaDate {
+
+  /**
+   * The day of the week (starting at 0)
+   */
+  day: number;
+
   /**
    * The day of the month (starting at 1)
    */
@@ -43,6 +49,19 @@ export class DatePickerComponent implements OnInit {
   readonly currentDate: MetaDate;
 
   /**
+   * Array of days of the week
+   */
+  readonly weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  /**
    * The full month for the current selected month/year
    */
   fullMonth: Array<MetaDate> = [];
@@ -58,13 +77,17 @@ export class DatePickerComponent implements OnInit {
     const endOfMonthDate = endOfMonth(beginningOfMonthDate);
 
     const fullMonth: Array<MetaDate> = [];
+    let dayOfMonth = beginningOfMonthDate.getDay();
     for (let i = 1; i <= endOfMonthDate.getDate(); i++) {
       fullMonth.push({
+        day: dayOfMonth,
         date: i,
         month,
         year,
         selectable: true
       });
+
+      dayOfMonth = dayOfMonth === 6 ? 0 : dayOfMonth + 1;
     }
 
     if (beginningOfMonthDate.getDay() > 0) {
@@ -72,6 +95,7 @@ export class DatePickerComponent implements OnInit {
       for (let i = beginningOfMonthDate.getDay(); i > 0; i--) {
         const subtractedDate = subDays(beginningOfMonthDate, i);
         lastMonthDates.push({
+          day: subtractedDate.getDay(),
           date: subtractedDate.getDate(),
           month: subtractedDate.getMonth(),
           year: subtractedDate.getFullYear(),
@@ -86,6 +110,7 @@ export class DatePickerComponent implements OnInit {
     while (fullMonth.length < 42) {
       const addedDate = addDays(endOfMonthDate, daysToAdd++);
       fullMonth.push({
+        day: addedDate.getDay(),
         date: addedDate.getDate(),
         month: addedDate.getMonth(),
         year: addedDate.getFullYear(),
