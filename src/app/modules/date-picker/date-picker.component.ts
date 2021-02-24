8ff10d3d33ae651d66969b69b2d73d7a0c8f52ea
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-02-24 12:01:09 -0500

    Add logic to calculate 42 day span representing a month

--- src/app/modules/date-picker/date-picker.component.ts
+++ src/app/modules/date-picker/date-picker.component.ts
@@ -1,5 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { addDays, endOfMonth, subDays } from 'date-fns';
 
+/**
+ * Interface representing a Date broken down by date/month/year
+ * with extended metadata
+ */
+declare interface MetaDate {
+  /**
+   * The day of the month (starting at 1)
+   */
+  date: number;
+
+  /**
+   * The month (starting at 0)
+   */
+  month: number;
+
+  /**
+   * The full year
+   */
+  year: number;
+
+  /**
+   * Whether or not the day is selectable
+   */
+  selectable: boolean;
+}
+
+/**
+ * Component to allow a user to input/select a date
+ */
 @Component({
   selector: 'wily-date-picker',
   templateUrl: './date-picker.component.html',
@@ -7,9 +37,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatePickerComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * The current Date broken down by day/month/year
+   */
+  readonly currentDate: MetaDate;
+
+  /**
+   * The full month for the current selected month/year
+   */
+  fullMonth: Array<MetaDate> = [];
+
+  /**
+   * Generate a 42 element array representing the input month's days with
+   * padding from the previous/following month
+   * @param month the month to generate dates for
+   * @param year the year to generate the month for
+   */
+  private static generateMonth(month: number, year: number): Array<MetaDate> {
+    const beginningOfMonthDate = new Date(year, month, 1);
+    const endOfMonthDate = endOfMonth(beginningOfMonthDate);
+
+    const fullMonth: Array<MetaDate> = [];
+    for (let i = 1; i <= endOfMonthDate.getDate(); i++) {
+      fullMonth.push({
+        date: i,
+        month,
+        year,
+        selectable: true
+      });
+    }
+
+    if (beginningOfMonthDate.getDay() > 0) {
+      const lastMonthDates: Array<MetaDate> = [];
+      for (let i = beginningOfMonthDate.getDay(); i > 0; i--) {
+        const subtractedDate = subDays(beginningOfMonthDate, i);
+        lastMonthDates.push({
+          date: subtractedDate.getDate(),
+          month: subtractedDate.getMonth(),
+          year: subtractedDate.getFullYear(),
+          selectable: false
+        });
+      }
+
+      fullMonth.unshift(...lastMonthDates);
+    }
+
+    let daysToAdd = 1;
+    while (fullMonth.length < 42) {
+      const addedDate = addDays(endOfMonthDate, daysToAdd++);
+      fullMonth.push({
+        date: addedDate.getDate(),
+        month: addedDate.getMonth(),
+        year: addedDate.getFullYear(),
+        selectable: false
+      });
+    }
+
+    return fullMonth;
+  }
+
+  constructor() {
+    const currentDate = new Date();
+    this.currentDate = {
+      date: currentDate.getDate(),
+      month: currentDate.getMonth(),
+      year: currentDate.getFullYear()
+    } as MetaDate;
+
+    this.fullMonth = DatePickerComponent.generateMonth(this.currentDate.month, this.currentDate.year);
+  }
 
   ngOnInit(): void {
   }
-
 }
