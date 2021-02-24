65a4df750b626dc3bc3829355d82e4e3d9d5d16c
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-02-24 13:27:10 -0500

    Hook up aria labels on date items

--- src/app/modules/date-picker/date-picker.module.ts
+++ src/app/modules/date-picker/date-picker.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatePickerComponent } from './date-picker.component';
+import { OrdinalNumberPipe } from '../ordinal-number-pipe/ordinal-number.pipe';
+import { OrdinalNumberPipeModule } from '../ordinal-number-pipe/ordinal-number-pipe.module';
 
 @NgModule({
   declarations: [DatePickerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    OrdinalNumberPipeModule
   ],
-  exports: [DatePickerComponent]
+  exports: [DatePickerComponent],
+  providers: [OrdinalNumberPipe]
 })
 export class DatePickerModule { }
