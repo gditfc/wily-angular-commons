6a4c9fb433621126fb00b91c75fd8eca6130cb34
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-02-25 12:19:07 -0500

    Add TODOs

--- src/app/modules/date-picker/date-picker.component.ts
+++ src/app/modules/date-picker/date-picker.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 /**
  * Component to allow a user to input/select a date
+ * TODO: position calendar either above/below input based on viewport/height
+ * TODO: reposition on screen resize
  */
 @Component({
   selector: 'wily-date-picker',
