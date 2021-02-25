caee10e4b6bf4317e2a28d599be3edbb238f4aeb
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-02-25 14:37:30 -0500

    First draft of calendar open positioning logic

--- src/app/modules/date-picker/date-picker.component.ts
+++ src/app/modules/date-picker/date-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 /**
  * Component to allow a user to input/select a date
@@ -12,11 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatePickerComponent implements OnInit {
 
+  /**
+   * ViewChild of the date picker div
+   */
+  @ViewChild('datePickerDiv')
+  datePickerDiv: ElementRef<HTMLDivElement>;
+
+  /**
+   * ViewChild of the calendar div
+   */
+  @ViewChild('calendarDiv')
+  calendarDiv: ElementRef<HTMLDivElement>;
+
+  /**
+   * ViewChild of the calendar overlay div
+   */
+  @ViewChild('calendarOverlayDiv')
+  calendarOverlayDiv: ElementRef<HTMLDivElement>;
+
+  /**
+   * Object representing the fixed dimensions of the calendar widget
+   */
+  readonly calendarDimensions = {
+    width: 300,
+    height: 340
+  };
+
+  private readonly calendarPadding = 5;
+
   showCalendar = false;
 
   console = console;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void { }
+
+  openCalendar(): void {
+    const {x, y, height} = this.datePickerDiv.nativeElement.getBoundingClientRect();
+    const datePickerBottomLeftAnchor = y + height;
+    const availableBottomSpace = window.innerHeight - datePickerBottomLeftAnchor;
+    const availableTopSpace = y;
+    const offsetCalendarHeight = this.calendarDimensions.height + this.calendarPadding;
+
+    if (availableBottomSpace > offsetCalendarHeight) {
+      this.renderer.setStyle(this.calendarDiv.nativeElement, 'transform-origin', 'top left');
+      this.renderer.setStyle(this.calendarDiv.nativeElement, 'left', `${x}px`);
+      this.renderer.setStyle(this.calendarDiv.nativeElement, 'top', `${datePickerBottomLeftAnchor + this.calendarPadding}px`);
+    } else if (availableTopSpace > offsetCalendarHeight) {
+      this.renderer.setStyle(this.calendarDiv.nativeElement, 'transform-origin', 'bottom left');
+      this.renderer.setStyle(this.calendarDiv.nativeElement, 'left', `${x}px`);
+      this.renderer.setStyle(this.calendarDiv.nativeElement, 'bottom', `${y - this.calendarPadding}px`);
+    }
+
+    this.renderer.setStyle(this.calendarOverlayDiv.nativeElement, 'display', '');
+    this.showCalendar = true;
+  }
+
+  handleCalendarClose(): void {
+    this.renderer.setStyle(this.calendarOverlayDiv.nativeElement, 'display', 'none');
+    this.showCalendar = false;
+  }
 }
