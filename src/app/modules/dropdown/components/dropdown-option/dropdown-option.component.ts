05c464d40e0413e28425155f851429960c92cae6
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-01 12:50:03 -0500

    Add dropdown option/optgroup component

---
+++ src/app/modules/dropdown/components/dropdown-option/dropdown-option.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-option',
+  templateUrl: './dropdown-option.component.html',
+  styleUrls: ['./dropdown-option.component.css']
+})
+export class DropdownOptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/dropdown/dropdown.module.ts
+++ src/app/modules/dropdown/dropdown.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent} from './dropdown.component';
+import { DropdownOptionComponent } from './components/dropdown-option/dropdown-option.component';
+import { DropdownOptgroupComponent } from './components/dropdown-optgroup/dropdown-optgroup.component';
 
 @NgModule({
-  declarations: [DropdownComponent],
+  declarations: [
+    DropdownComponent,
+    DropdownOptionComponent,
+    DropdownOptgroupComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [DropdownComponent]
+  exports: [
+    DropdownComponent,
+    DropdownOptionComponent,
+    DropdownOptgroupComponent
+  ]
 })
 export class DropdownModule { }
