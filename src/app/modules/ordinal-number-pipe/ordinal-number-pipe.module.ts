e2f46ec9575bd020b9c887fc5c6af45d029871b8
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-04 15:55:07 -0500

    Delete unused CSS file

--- src/app/modules/ordinal-number-pipe/ordinal-number-pipe.module.ts
+++ src/app/modules/ordinal-number-pipe/ordinal-number-pipe.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdinalNumberPipe } from './ordinal-number.pipe';
 
-
-
 @NgModule({
   declarations: [OrdinalNumberPipe],
   imports: [
