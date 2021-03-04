d96fc41f9e87c301277ef8db5b767dc2f5aef5cb
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-04 16:55:56 -0500

    Hook up notification component

--- src/app/modules/notification/notification.component.ts
+++ src/app/modules/notification/notification.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Notification } from './models/notification.model';
+import { NotificationService } from './services/notification.service';
 
+/**
+ * Component for displaying notifications
+ */
 @Component({
   selector: 'wily-notification',
   templateUrl: './notification.component.html',
@@ -7,9 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Array of notifications as an Observable
+   */
+  readonly notifications$: Observable<Array<Notification>> = this.service.getNotifications();
 
-  ngOnInit(): void {
+  /**
+   * Map of notification severity to color class
+   */
+  readonly severityColorMap = {
+    'success': 'bg_green',
+    'warn': 'bg_orange',
+    'error': 'bg_red',
+    'info': 'bg_blue'
+  };
+
+  /**
+   * Map of notification severity to icon class
+   */
+  readonly severityIconMap = {
+    'success': 'fa-check-circle',
+    'warn': 'fa-exclamation-circle',
+    'error': 'fa-times-circle',
+    'info': 'fa-info-circle'
+  };
+
+  /**
+   * Dependency injection site
+   * @param service the NotificationService
+   */
+  constructor(private service: NotificationService) { }
+
+  /**
+   * Init component
+   */
+  ngOnInit(): void { }
+
+  /**
+   * Delete a notification
+   * @param id the ID of the notification to delete
+   */
+  deleteNotification(id: string): void {
+    this.service.deleteNotification(id);
   }
-
 }
