64bfa88200a111bb010d2303563014df8d444c93
Brooks <frederic.peffer@gdit.com>
2021-03-05 11:01:55 -0500

    tweak notification animation

--- src/app/modules/notification/notification.component.ts
+++ src/app/modules/notification/notification.component.ts
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 import { Notification } from './models/notification.model';
 import { NotificationService } from './services/notification.service';
 
-const query = (s,a,o={optional:true})=>q(s,a,o);
+const query = (s, a, o= {optional: true}) => q(s, a, o);
 
 /**
  * Component for displaying notifications
@@ -35,13 +35,13 @@ const query = (s,a,o={optional:true})=>q(s,a,o);
       // cubic-bezier for a tiny bouncing feel
       transition(':enter', [
         style({ transform: 'scale(0.5)', opacity: 0 }),
-        animate('1s cubic-bezier(.8,-0.6,0.2,1.5)',
+        animate('0.75s cubic-bezier(.8,-0.6,0.2,1.5)',
           style({ transform: 'scale(1)', opacity: 1 }))
       ]),
       transition(':leave', [
         style({ transform: 'scale(1)', opacity: 1, height: '*' }),
-        animate('1s cubic-bezier(.8,-0.6,0.2,1.5)',
-          style({ transform: 'scale(0.5)', opacity: 0, height: '0px', margin: '0px' }))
+        animate('0.75s cubic-bezier(.8,-0.6,0.2,1.5)',
+          style({ transform: 'scale(0.5)', opacity: 0, height: '0px', margin: '0px', padding: '0px' }))
       ]),
     ])
   ]
