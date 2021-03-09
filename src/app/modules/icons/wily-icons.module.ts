f7e954c3310e857a1819781a1d5b195095e70337
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-09 16:06:07 -0500

    Replace pTooltip with wilyTooltip in icons module

--- src/app/modules/icons/wily-icons.module.ts
+++ src/app/modules/icons/wily-icons.module.ts
@@ -1,13 +1,13 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {WilyIconsRegular} from './models/wily-icons-regular.model';
-import {WilyIconsLight} from './models/wily-icons-light.model';
-import {WilyIconsSolid} from './models/wily-icons-solid.model';
-import {IconSelectComponent} from './components/icon-select.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TooltipModule} from 'primeng/tooltip';
-import {WilyDialogLegacyModule} from '../dialog-legacy/dialog-legacy.module';
-import {WilyDialogModule} from '../dialog/dialog.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TooltipModule } from '../tooltip/tooltip.module';
+import { WilyIconsRegular } from './models/wily-icons-regular.model';
+import { WilyIconsLight } from './models/wily-icons-light.model';
+import { WilyIconsSolid } from './models/wily-icons-solid.model';
+import { IconSelectComponent } from './components/icon-select.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { WilyDialogLegacyModule } from '../dialog-legacy/dialog-legacy.module';
+import { WilyDialogModule } from '../dialog/dialog.module';
 
 /**
  * Reference to the Font Awesome variable to add icons
