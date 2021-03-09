5b22873af7f5b8a352c417429532c7b6d87b9e36
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-09 16:22:06 -0500

    Update API exports

--- src/public_api.ts
+++ src/public_api.ts
@@ -9,6 +9,15 @@ export * from './app/modules/icons/wily-icons.module';
 export * from './app/modules/endpoint-state/endpoint-state.module';
 export * from './app/modules/dialog/dialog.module';
 export * from './app/modules/user-idle/wily-user-idle.module';
+export * from './app/modules/date-picker/date-picker.module';
+export * from './app/modules/dropdown/dropdown.module';
+export * from './app/modules/multi-select/multi-select.module';
+export * from './app/modules/ordinal-number-pipe/ordinal-number-pipe.module';
+export * from './app/modules/popover/popover.module';
+export * from './app/modules/tooltip/tooltip.module';
+
+/** DIRECTIVES */
+export * from './app/modules/tooltip/tooltip.directive';
 
 /** COMPONENTS */
 export * from './app/modules/icons/components/icon-select.component';
@@ -18,6 +27,11 @@ export * from './app/modules/push-container/push-container.component';
 export * from './app/modules/rich-text/rich-text.component';
 export * from './app/modules/endpoint-state/components/endpoint-state.component';
 export * from './app/modules/dialog/dialog.component';
+export * from './app/modules/date-picker/date-picker.component';
+export * from './app/modules/date-picker/components/calendar/calendar.component';
+export * from './app/modules/dropdown/dropdown.component';
+export * from './app/modules/multi-select/multi-select.component';
+export * from './app/modules/popover/popover.component';
 
 /** INTERCEPTORS */
 export * from './app/shared/interceptors/json.interceptor';
@@ -29,6 +43,9 @@ export * from './app/shared/services/local-storage.service';
 export * from './app/shared/services/cognito-auth.service';
 export * from './app/shared/services/dialog.service';
 export * from './app/modules/user-idle/services/user-idle.service';
+
+/** PIPES */
+export * from './app/modules/ordinal-number-pipe/ordinal-number.pipe';
 
 /** MODELS */
 export * from './app/modules/paginator/pagination-event.model';
@@ -44,6 +61,10 @@ export * from './app/shared/models/chart/pie-chart.model';
 export * from './app/shared/models/chart/stacked-bar-chart.model';
 export * from './app/shared/models/chart/state-choropleth.model';
 export * from './app/modules/user-idle/models/user-idle-config.model';
+export * from './app/modules/dropdown/models/dropdown-option.model';
+export * from './app/modules/dropdown/models/dropdown-option-group.model';
+export * from './app/modules/multi-select/models/multi-select-option.model';
+export * from './app/modules/multi-select/models/multi-select-option-group.model';
 
 /** ENUMS */
 export * from './app/shared/enums/endpoint-status.enum';
