b898330d6c000b772f4e55e4bc7bcf6c55350274
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-11 13:13:07 -0500

    Add week picker module

---
+++ src/app/modules/week-picker/week-picker.component.ts
@@ -0,0 +1,80 @@
+import { AnimationEvent } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/index';
+
+@Component({
+  selector: 'wily-week-picker',
+  templateUrl: './week-picker.component.html',
+  styleUrls: ['./week-picker.component.css']
+})
+export class WeekPickerComponent implements OnInit {
+
+  /**
+   * Class list to apply to the date range input
+   */
+  @Input()
+  inputClassList = 'input md overlay_alt2 page_container_color';
+
+  /**
+   * Class list to apply to the week picker button
+   */
+  @Input()
+  calendarButtonClassList = 'overlay_alt2 bg_blue_alt';
+
+  /**
+   * Whether or not the week picker is disabled
+   */
+  @Input()
+  disabled = false;
+
+  /**
+   * The aria label to apply to the date range input
+   */
+  @Input()
+  ariaLabel = 'Date';
+
+  /**
+   * The current date
+   */
+  readonly currentDate: any = { selectable: true };
+
+  /**
+   * Array of days of the week
+   */
+  readonly weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  readonly _selectedMonth = new BehaviorSubject(0);
+
+  readonly _selectedYear = new BehaviorSubject(1970);
+
+  readonly fullMonth$: any;
+
+  /**
+   * Whether or not to render the week picker
+   */
+  render = false;
+
+  /**
+   * Whether or not to show the week picker
+   */
+  showCalendar = false;
+
+  selectedDate: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAnimationStart(event: AnimationEvent): void { }
+
+  onAnimationDone(event: AnimationEvent): void { }
+}
---
+++ src/app/modules/week-picker/week-picker.module.ts
@@ -0,0 +1,17 @@
+import { A11yModule } from '@angular/cdk/a11y';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OrdinalNumberPipeModule } from '../ordinal-number-pipe/ordinal-number-pipe.module';
+import { WeekPickerComponent } from './week-picker.component';
+
+@NgModule({
+  declarations: [WeekPickerComponent],
+  imports: [
+    CommonModule,
+    OrdinalNumberPipeModule,
+    FormsModule,
+    A11yModule
+  ]
+})
+export class WilyWeekPickerModule { }
