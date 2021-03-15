cb81dca20503b82d18b21d80352c08cbea17461e
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-15 09:49:02 -0400

    Add method get filtered Wily icons

--- src/app/modules/icons/services/icons.service.ts
+++ src/app/modules/icons/services/icons.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { WilyIconsLight } from '../models/wily-icons-light.model';
+import { WilyIconsRegular } from '../models/wily-icons-regular.model';
+import { WilyIconsSolid } from '../models/wily-icons-solid.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class IconsService {
-
   constructor() { }
+
+  /**
+   * Get all Wily icons filtered by icon type
+   * @param filter the icon type to filter by
+   * @private
+   */
+  private getWilyIcons(filter: 'all' | 'solid' | 'regular' | 'light'): Array<string> {
+    const icons: Array<string> = [];
+
+    if (filter === 'all' || filter === 'solid') {
+      icons.push(...WilyIconsSolid.icons.map(icon => `${icon.prefix}-${icon.iconName}`));
+    }
+
+    if (filter === 'all' || filter === 'regular') {
+      icons.push(...WilyIconsRegular.icons.map(icon => `${icon.prefix}-${icon.iconName}`));
+    }
+
+    if (filter === 'all' || filter === 'light') {
+      icons.push(...WilyIconsLight.icons.map(icon => `${icon.prefix}-${icon.iconName}`));
+    }
+
+    return icons;
+  }
 }
