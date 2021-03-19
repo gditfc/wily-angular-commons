f8903684eaa7343390528729760d87204721c281
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-03-19 16:49:46 -0400

    Started implementing color picker component validation logic

--- src/app/modules/color-picker/color-picker.component.ts
+++ src/app/modules/color-picker/color-picker.component.ts
@@ -1,5 +1,5 @@
-import { Component, forwardRef, OnInit, Renderer2 } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ChangeDetectorRef, Component, forwardRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 /**
  * Component that wraps the native HTML5 color picker for supported browsers
@@ -16,7 +16,76 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
     }
   ]
 })
-export class ColorPickerComponent implements OnInit {
+export class ColorPickerComponent implements ControlValueAccessor, OnInit {
+
+  /**
+   * Regex to match a hexadecimal string (#000000)
+   * @private
+   */
+  private static readonly FULL_HEX_REGEX = new RegExp('^#[0-9A-Fa-f]{6}$');
+
+  /**
+   * Regex to match a short hexadecimal string (#000)
+   * @private
+   */
+  private static readonly SHORT_HEX_REGEX = new RegExp('^#[0-9A-Fa-f]{3}$');
+
+  /**
+   * Regex to match a valid hex character (#, A-F, a-f, 0-9)
+   * @private
+   */
+  private static readonly HEX_CHARACTER_REGEX = new RegExp('^[#0-9A-Fa-f]$');
+
+  /**
+   * Navigation keys
+   * @private
+   */
+  private static readonly NAVIGATION_KEYS = [
+    'Enter', 'Esc', 'Escape', 'Tab', 'Shift',
+    'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown',
+    'Backspace', 'Delete'
+  ];
+
+  /**
+   * Set the value of the color picker
+   * @param value the value to set
+   */
+  @Input('value')
+  set value(value: string) {
+    let hexString = value;
+
+    if (hexString) {
+      ColorPickerComponent.FULL_HEX_REGEX.lastIndex = 0;
+      ColorPickerComponent.SHORT_HEX_REGEX.lastIndex = 0;
+
+      hexString = hexString.startsWith('#') ? hexString : `#${hexString}`;
+
+      if (ColorPickerComponent.SHORT_HEX_REGEX.test(hexString)) {
+        let fullHexString = '';
+
+        for (let i = 1; i <= 3; i++) {
+          fullHexString += `${hexString[i]}${hexString[i]}`;
+        }
+
+        hexString = `#${fullHexString}`;
+      }
+
+      if (!ColorPickerComponent.FULL_HEX_REGEX.test(hexString)) {
+        hexString = '#000000';
+      }
+    }
+
+    this._value = hexString;
+  }
+
+  /**
+   * Set disabled
+   * @param disabled whether or not the control should be disabled
+   */
+  @Input('disabled')
+  set disabled(disabled: boolean) {
+    this._disabled = disabled;
+  }
 
   /**
    * Whether or not to render the component
@@ -29,10 +98,32 @@ export class ColorPickerComponent implements OnInit {
   colorPickerSupported: boolean;
 
   /**
+   * The value of the color picker
+   */
+  _value: string;
+
+  /**
+   * Whether or not the color picker is disabled
+   * @private
+   */
+  private _disabled = false;
+
+  /**
+   * Function to call on change
+   */
+  onChange: (value: any) => void = () => {};
+
+  /**
+   * Function to call on touch
+   */
+  onTouched: () => any = () => {};
+
+  /**
    * Dependency injection site
    * @param renderer the Angular renderer
+   * @param changeDetectorRef the Angular ChangeDetectorRef
    */
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2, private changeDetectorRef: ChangeDetectorRef) { }
 
   /**
    * Init component, check for color picker browser support
@@ -41,6 +132,96 @@ export class ColorPickerComponent implements OnInit {
     this.colorPickerSupported = this.isColorPickerSupported();
     this.render = true;
   }
+
+  /**
+   * Write value
+   * @param value the value to write
+   */
+  writeValue(value: string): void {
+    if (!this._disabled) {
+      this.value = value;
+    }
+
+    this.changeDetectorRef.markForCheck();
+  }
+
+  /**
+   * Register on change function
+   * @param fn the function to register
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Register on touched function
+   * @param fn the function to register
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Set whether or not the control should be disabled
+   * @param isDisabled disabled or not
+   */
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+    this.changeDetectorRef.markForCheck();
+  }
+
+  /**
+   * Prevent illegal input
+   * @param event the keydown KeyboardEvent
+   */
+  handleKeyDown(event: KeyboardEvent): void {
+    const { key } = event;
+
+    if (!ColorPickerComponent.NAVIGATION_KEYS.includes(key)) {
+      ColorPickerComponent.HEX_CHARACTER_REGEX.lastIndex = 0;
+
+      const preventDefault = !ColorPickerComponent.HEX_CHARACTER_REGEX.test(key) ||
+        (key === '#' && (event.target as HTMLInputElement).selectionStart > 0) ||
+        event.target['value'].length >= 7;
+
+      if (preventDefault) {
+        event.preventDefault();
+      }
+    }
+  }
+
+  /**
+   * Update model on input
+   * @param event
+   */
+  handleInput(event: Event): void {
+    this.updateModel(event.target['value']);
+  }
+
+  /**
+   * Update model on paste
+   * @param event
+   */
+  handleChange(event: Event): void {
+    this.updateModel(event.target['value']);
+  }
+
+  /**
+   * Update model
+   * @param value the new value of the model
+   * @private
+   */
+  private updateModel(value: string): void {
+    if (!this._disabled) {
+      const valueBeforeUpdate = this._value;
+      this.value = value;
+
+      if (valueBeforeUpdate !== this._value) {
+        this.onChange(value);
+      }
+    }
+  }
+
 
   /**
    * Determine if the browser supports the HTML5 color picker
