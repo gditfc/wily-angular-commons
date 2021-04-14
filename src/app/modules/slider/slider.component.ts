b6ad10cf8298f2de83077645bffc3505d3f68b79
Matthew Pigliavento <matthew.pigliavento@gdit.com>
2021-04-14 11:23:11 -0400

    Hook up slider control value accessor

--- src/app/modules/slider/slider.component.ts
+++ src/app/modules/slider/slider.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, forwardRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 /**
  * Component that wraps the native HTML5 slider
@@ -6,9 +7,31 @@ import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'wily-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SliderComponent),
+      multi: true
+    }
+  ]
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements ControlValueAccessor, OnInit {
+
+  /**
+   * ViewChild of the slider input
+   */
+  @ViewChild('sliderInput')
+  sliderInput: ElementRef<HTMLInputElement>;
+
+  /**
+   * Set the value of the component
+   * @param value the value to set
+   */
+  @Input()
+  set value(value: number) {
+    this._value = value;
+  }
 
   /**
    * Set the minimum slider value
@@ -38,6 +61,15 @@ export class SliderComponent implements OnInit {
   }
 
   /**
+   * Set the disabled state of the slider/input
+   * @param disabled the value to set
+   */
+  @Input()
+  set disabled(disabled: boolean) {
+    this._disabled = disabled;
+  }
+
+  /**
    * The unit being represented by the slider
    */
   @Input()
@@ -50,19 +82,39 @@ export class SliderComponent implements OnInit {
   ariaLabel: string;
 
   /**
+   * The internal value of the component
+   */
+  _value: number;
+
+  /**
    * The minimum slider value
    */
-  _min: number;
+  _min = 0;
 
   /**
    * The maximum slider value
    */
-  _max: number;
+  _max = 100;
 
   /**
    * The step size of the slider
    */
-  _step: number;
+  _step = 1;
+
+  /**
+   * Whether the slider is disabled or not
+   */
+  _disabled: boolean;
+
+  /**
+   * Function to call on change
+   */
+  onChange: (value: any) => void = () => {};
+
+  /**
+   * Function to call on touch
+   */
+  onTouched: () => any = () => {};
 
   /**
    * Dependency injection site
@@ -71,7 +123,78 @@ export class SliderComponent implements OnInit {
   constructor(private changeDetectorRef: ChangeDetectorRef) { }
 
   /**
+   * Get the value of the component
+   */
+  get value(): number {
+    return this._value;
+  }
+
+  /**
    * Init component
    */
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    if (isNaN(this.value)) {
+      this.value = isNaN(this._min) ? 0 : this._min;
+    }
+  }
+
+  /**
+   * Set internal value when external value changes
+   * @param value the external value
+   */
+  writeValue(value: number): void {
+    this.value = value;
+    this.changeDetectorRef.markForCheck();
+  }
+
+  /**
+   * Register onChange function
+   * @param fn the function to register
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Register onTouched function
+   * @param fn the function to register
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Set the disabled state of the component
+   * @param isDisabled disabled or not
+   */
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+    this.changeDetectorRef.markForCheck();
+  }
+
+  /**
+   * Update value on input
+   * @param value the new value of the component
+   */
+  handleInput(value: number): void {
+    let valueToSet: number;
+
+    if (isNaN(value) || value < this._min) {
+      valueToSet = this._min;
+    } else {
+      valueToSet = Math.min(this._max, value);
+    }
+
+    this.value = valueToSet;
+    this.sliderInput.nativeElement.value = String(valueToSet);
+    this.onChange(valueToSet);
+  }
+
+  /**
+   * Prevent the paste event from completing
+   * @param event the paste ClipboardEvent
+   */
+  preventPaste(event: ClipboardEvent): void {
+    event.preventDefault();
+  }
 }
--- src/app/modules/slider/slider.module.ts
+++ src/app/modules/slider/slider.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SliderComponent } from './slider.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { SliderComponent } from './slider.component';
     SliderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class WilySliderModule { }
