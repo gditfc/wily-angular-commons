1b16ae848f60493e0ca3f4ea9d98627a44bb97a0
Eileen <eileen.bohen@gdit.com>
2022-02-14 12:10:37 -0500

    add new module to app module

--- src/app/modules/new-tab-warning/wily-new-tab-warning.module.ts
+++ src/app/modules/new-tab-warning/wily-new-tab-warning.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewTabWarningComponent } from './new-tab-warning.component';
 
-
-
 @NgModule({
   declarations: [NewTabWarningComponent],
   imports: [
     CommonModule
   ]
 })
-export class NewTabWarningModule { }
+export class WilyNewTabWarningModule { }
